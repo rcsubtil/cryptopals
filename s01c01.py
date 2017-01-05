d1c175e64ea04a54ad37e24c9e1bea0b53041376
Rodrigo Cubo Subtil <rc.subtil@gmail.com>
2017-01-05 21:54:46 -0200

    little improvements

--- s01c01.py
+++ s01c01.py
@@ -8,7 +8,10 @@
 # Should produce the following base64 sequence:
 # SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
 
+print "\nthe sequence: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 hexseq = raw_input("input your hex sequence -> ")
+
+print "\nshould produce: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 base64seq = hexseq.decode("hex").encode("base64")
 
 print ("your base64 sequence -> %s") % base64seq
