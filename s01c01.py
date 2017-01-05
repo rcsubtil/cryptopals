61a5bc189e49838b19f61ac27f6a05743f7bab0e
Rodrigo Cubo Subtil <rc.subtil@gmail.com>
2017-01-05 21:59:18 -0200

    commenting

--- s01c01.py
+++ s01c01.py
@@ -2,12 +2,6 @@
 
 # Convert hex to base64
 
-# The following hex sequence:
-# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-
-# Should produce the following base64 sequence:
-# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
-
 print "\nthe sequence: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 hexseq = raw_input("input your hex sequence -> ")
 
